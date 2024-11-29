38b21a62df10740d8cbd928ce2d19efd4b872f2b
Mathias2812 <mathias.sparre2812@gmail.com>
2024-11-29 11:31:20 +0100

    fix core event

--- Core/Models/Events.cs
+++ Core/Models/Events.cs
@@ -1,6 +1,7 @@
 using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,20 @@ namespace Core.Models
     public class Events
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        [Required]
         public string Location { get; set; }
+        [Required]
         public string Food { get; set; }
-        
-        public string Company  { get; set; }
-        
+        [Required]
+        public string Company { get; set; } = "Arla";
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Participants must be greater than 0.")]
         public int Participants { get; set; }
+        public string Requests { get; set; }
         
     }
 }
