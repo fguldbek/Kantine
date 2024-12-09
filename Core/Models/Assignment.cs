24562fcba9e5612364956f131720096ad681ecde
Mathias2812 <mathias.sparre2812@gmail.com>
2024-12-09 12:12:30 +0100

    req

--- Core/Models/Assignment.cs
+++ Core/Models/Assignment.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 
 namespace Core.Models;
@@ -5,14 +6,25 @@ namespace Core.Models;
 
 public class Assignment
 {
-    public int Id { get; set; }  // MongoDB will automatically generate this ID
+    public int Id { get; set; }  // MongoDB generates this ID
 
+    [Required(ErrorMessage = "Employee is required.")]
     public int EmployeeId { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100, ErrorMessage = "Name must not exceed 100 characters.")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "Start date is required.")]
     public DateTime StartDate { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "End date is required.")]
     public DateTime EndDate { get; set; } = DateTime.Now;
-    public bool Status { get; set; }
-    
+
+    [Required(ErrorMessage = "Description is required.")]
+    [StringLength(500, ErrorMessage = "Description must not exceed 500 characters.")]
     public string Description { get; set; }
+    
+    public bool Status { get; set; }
     
 }
